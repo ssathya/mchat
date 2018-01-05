239587329ead25daea9c3fa0c0f98911a9300716
Sridhar Sathya <sridhar_0314@live.com>
2018-01-05 14:27:46 -0500

    Added Clear functionality Added Clear functionality to reset radio buttons

--- mchat/src/app/qa-form/qa-form.component.ts
+++ mchat/src/app/qa-form/qa-form.component.ts
@@ -1,7 +1,7 @@
 import { QaFormServiceService } from './../qa-form-service.service';
 import { QaTemplate, Answers } from './../qa-template';
 import { Component, OnInit } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-qa-form',
@@ -10,43 +10,48 @@ import {MatTabsModule} from '@angular/material/tabs';
 })
 export class QaFormComponent implements OnInit {
   public questAndAns: QaTemplate[];
-  public answeredQuestions:number;
-  public showAlert : boolean;
-  public score : number;
-  public showQuestions : number;
-  constructor(formService:QaFormServiceService) {
-    this.questAndAns=formService.BuildQA();
-    this.answeredQuestions=0;
-    this.showAlert=false;
-    this.showQuestions=0;
-    this.score=23;
-   }
+  public answeredQuestions: number;
+  public showAlert: boolean;
+  public score: number;
+  public showQuestions: number;
+  constructor(formService: QaFormServiceService) {
+    this.questAndAns = formService.BuildQA();
+    this.answeredQuestions = 0;
+    this.showAlert = false;
+    this.showQuestions = 0;
+    this.score = 23;
+  }
 
-   public radioChangeHandler(event: any){
-     var id = event.target.id;
-     this.score=23;
-     if (this.questAndAns[id].ResponseAnswer === Answers.Undefined){
+  public radioChangeHandler(event: any) {
+    var id = event.target.id;
+    this.score = 23;
+    if (this.questAndAns[id].ResponseAnswer === Answers.Undefined) {
       this.answeredQuestions++;
-     }
-     if (event.target.value==="0") {
-      this.questAndAns[id].ResponseAnswer= Answers.No;  
-     }
-     else{
-      this.questAndAns[id].ResponseAnswer=Answers.Yes;
-     }     
-//     console.log(event);
-   }
-   public Evaluate(){     
-     for (var i = 0; i < this.questAndAns.length; i++)
-     {
-       if (this.questAndAns[i].ExpectedAnswer===this.questAndAns[i].ResponseAnswer){
-         this.score--;
-       }
-     }     
-   }
-   public ToggleQuestions(){
-     this.showQuestions = this.showQuestions === 0? 1:0;
-   }
+    }
+    if (event.target.value === "0") {
+      this.questAndAns[id].ResponseAnswer = Answers.No;
+    }
+    else {
+      this.questAndAns[id].ResponseAnswer = Answers.Yes;
+    }
+    //     console.log(event);
+  }
+  public Evaluate() {
+    for (var i = 0; i < this.questAndAns.length; i++) {
+      if (this.questAndAns[i].ExpectedAnswer === this.questAndAns[i].ResponseAnswer) {
+        this.score--;
+      }
+    }
+  }
+  public ToggleQuestions() {
+    this.showQuestions = this.showQuestions === 0 ? 1 : 0;
+  }
+  public clearInput() {
+    this.score = 23;
+    for (var i = 0; i < this.questAndAns.length; i++) {
+      this.questAndAns[i].ResponseAnswer = Answers.Undefined;
+    }
+  }
   ngOnInit() {
   }
 
